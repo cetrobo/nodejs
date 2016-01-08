41059bdb46859cd96c4fa0626064803a24429eaf
cetrobo <achraf.benaomar@gmail.com>
2016-01-08 01:12:27 +0100

    adding scroling up

--- script/index.js
+++ script/index.js
@@ -1,22 +1,9 @@
 $(document ).ready(function() {
-
-    $(window).scroll(function() {
-        
-        $('.section-title').addClass('animated fadeInLeft');
-        $('.line-section').addClass('animated fadeInLeft');
-        $('p.section-contenue').addClass('animated fadeIn');
-        $('.section-thumb').addClass('animated fadeInRight');
-        $('.title-section2').addClass('animated fadeInLeft');
-        $('.exp-title').addClass('animated fadeIn');
-        $('.line-section2').addClass('animated fadeInLeft');
-        $('.aboutme-section2').addClass('animated fadeIn');
-        $('.title-work').addClass('animated fadeInLeft');
-        $('.border-works').addClass('animated fadeInLeft');
-        $('.first-title').addClass('animated fadeIn');
-        $('.sujet-work').addClass('animated fadeIn');
-        $('.img-thumb').addClass('animated fadeInLeft');
-        $('.msg-title').addClass('animated fadeIn');    
-        
-    });      
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
     
     });
