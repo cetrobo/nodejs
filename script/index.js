1cd3ed741ae94f58dbec4cba812da0dbb42b9c42
cetrobo <achraf.benaomar@gmail.com>
2016-01-11 14:58:57 +0100

    edite some file

--- script/index.js
+++ script/index.js
@@ -1,28 +1,6 @@
 $(document).ready(function() {
-    $('.post-logo').addClass('animated fadeInUp');
-    $('.post.title').addClass('animated fadeInUp');
-    $('.post.title2').addClass('animated fadeInUp');
-         $(window).scroll( function(){
-            $('.section-title').addClass('animated fadeInUp');
-             $('.section-title').addClass('animated fadeInUp');
-             $('.line-section').addClass('animated fadeInLeftBig');
-             $('.section-contenue').addClass('animated bounceInUp');
-             $('.section-thumb').addClass('animated fadeInUp');
-             $('.title-section2').addClass('animated fadeInLeft');
-             $('.exp-title').addClass('animated fadeInUp');
-             $('.line-section2').addClass('animated fadeInLeft');
-             $('.aboutme-section2').addClass('animated fadeInUp');
-              $('.title-work').addClass('animated fadeInLeft');
-             $('.border-works').addClass('animated fadeInLeft');
-             $('.first-title').addClass('animated fadeInLeft');
-             $('.sujet-work').addClass('animated fadeInUp');
-              $('.img-thumb').addClass('animated fadeInLeft');
-             $('.msg-title').addClass('animated fadeInUp');
-             $('#social').addClass('animated fadeInUp');
-             $('#thanks').addClass('animated fadeInLeft');
-             $('.img-thumb2 img').addClass('animated fadeInUp');
-             $('#thanks h1').addClass('animated fadeInLeft');
-        });
+  
+       
     $('.scrollup').click(function () {
         $("html, body").animate({
             scrollTop: 0
