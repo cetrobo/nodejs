1368d1b939e6ccb4bf731367d0558bf337261211
cetrobo <achraf.benaomar@gmail.com>
2016-01-12 01:38:56 +0100

    scroll

--- script/index.js
+++ script/index.js
@@ -1,21 +1,42 @@
 $(document).ready(function() {
     $('.post-logo').addClass('animated fadeInUp');
-    $('.post.title').addClass('animated fadeInUp');
-    $('.post.title2').addClass('animated fadeInUp');
+    $('.title').addClass('animated fadeInUp');
   
          $(window).scroll( function(){
-        
-          if($('.section-title:in-viewport')){
-                
+          
+            $('#header').css("opacity", 1 - $(window).scrollTop() / 550);
+           
+           
+          if($('#section:in-viewport'))
+              
                  $('.section-title').addClass('animated fadeInUp');
                  $('.line-section').addClass('animated fadeInLeft');
                  $('.section-contenue').addClass('animated fadeInUp');
                  $('.section-thumb').addClass('animated fadeInUp');
-                  
             
-          }
-            
+           
+              
+          if($('.title-section2:in-viewport'))
+                
+            $('.title-section2').addClass('animated fadeInLeft');
+            $('.exp-title').addClass('animated fadeInUp');
+            $('.line-section2').addClass('animated fadeInLeft');
+            $('.aboutme-section2').addClass('animated fadeInUp');
+                 
          
+           
+           if($('.title-work:in-viewport'))
+                
+            $('.title-work').addClass('animated fadeInUp');
+            $('border-works').addClass('animated fadeInLeft');
+            $('.first-title').addClass('animated fadeInUp');
+            $('.sujet-work').addClass('animated fadeInUp');
+            $('.sujet-works').addClass('animated fadeInUp');
+            $('.thumbs').addClass('animated fadeInLeft');
+            $('.thumb').addClass('animated fadeInUp');
+                 
+         
+           
         });
     $('.scrollup').click(function () {
         $("html, body").animate({
